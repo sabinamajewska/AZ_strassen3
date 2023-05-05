0c8dbf263d91a7a3d583a8ed04aa8b169778d184
Sabina <sabinaduchnowska@gmail.com>
2023-05-05 18:45:51 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from numpy import matrix
+def load_matrix_A_from_file(filepath):
+    loaded_matrix_A = []  # to będzie nasza macierz A
+    with open(filepath) as f:
+        for line in f.readlines():
+            line_ints = [int(s) for s in line.split(' ')]  # rzutujemy linię wejścia na listę intów
+            loaded_matrix_A.append(line_ints)
+    return matrix(loaded_matrix_A) # funkcja matrix z listy list robi reprezentację macierzową
+def load_matrix_B_from_file(filepath):
+    loaded_matrix_B = []  # to będzie nasza macierz B
+    with open(filepath) as f:
+        for line in f.readlines():
+            line_ints = [int(s) for s in line.split(' ')]  # rzutujemy linię wejścia na listę intów
+            loaded_matrix_B.append(line_ints)
+    return matrix(loaded_matrix_B) # funkcja matrix z listy list robi reprezentację macierzową
+
+def isSquare (matrix):
+    return len(matrix) == len(matrix.T) # sprawdzam czy liczba wierszy w macierzy A i w macierzy A transponowanej jest równa
+
+
+A = load_matrix_B_from_file("matrix_example_1.txt")
+B = load_matrix_A_from_file("matrix_example_2.txt")
+print(A)
+print(B)
+print(isSquare(A))
+print(isSquare(B))
