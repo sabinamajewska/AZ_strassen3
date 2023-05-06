51825d9bd2b77550a574886740d92c9fc0caa942
Sabina <sabinaduchnowska@gmail.com>
2023-05-06 18:53:45 +0200

    Dodałam wykorzystanie funkcji pomocniczej adjust_size w funkcji split

--- functions.py
+++ functions.py
@@ -18,26 +18,6 @@ def load_matrix_B_from_file(filepath):
 def is_square (matrix):
     return len(matrix) == len(matrix.T) # sprawdzam czy liczba wierszy w macierzy A i w macierzy A transponowanej jest równa
 
-# DOPISAĆ: funkcja dzieląca macierz na bloki
-def split(matrix):
-    size = len(matrix)
-    power = 1
-    while (size > power):
-        power = power * 3
-    if size == power:
-        A11 = matrix[0:int(size/3), 0:int(size/3)]
-        A12 = matrix[0:int(size/3), int(size/3):int(2*size/3)]
-        A13 = matrix[0:int(size/3), int(2*size/3):int(size)]
-        A21 = matrix[int(size/3):int(2*size/3), 0:int(size/3)]
-        A22 = matrix[int(size/3):int(2*size/3), int(size/3):int(2*size/3)]
-        A23 = matrix[int(size/3):int(2*size/3), int(2*size/3):int(size)]
-        A31 = matrix[int(2*size/3):int(size), 0:int(size/3)]
-        A32 = matrix[int(2*size/3):int(size), int(size/3):int(2*size/3)]
-        A33 = matrix[int(2*size/3):int(size), int(2*size/3):int(size)]
-        return A11, A12, A13, A21, A22, A23, A31, A32, A33
-    else:
-        print("Macierz nie jest wymiaru 3^n")
-
 def adjust_size (matrix_example): #sprawdzam czy dana macierz kwadratowa jest rozmiaru 3^k
     size = len(matrix_example)
     power = 1
@@ -51,11 +31,21 @@ def adjust_size (matrix_example): #sprawdzam czy dana macierz kwadratowa jest ro
         result[0:len(matrix_example), 0:len(matrix_example)] = matrix_example
     return result
 
-def save_matrix_to_file(matrix_example, filepath): # funkcja zapisująca macierz do pliku
-    f = open(filepath, "w")  # "w" oznacza, że jeżeli plik nie isnieje, to zostanie utworozny, jeżeli istnieje, to go nadpiszemy
-    f.write(str(adjust_size(matrix_example))) #tutaj akurat zapisuję macierz powstałą w funkcji pomocniczej
-    f.close()
-    return
+def split(matrix_example):
+    matrix_example = adjust_size(matrix_example) #chcemy dzielić macierze postaci 3^k
+    size = len(matrix_example)
+
+    A11 = matrix_example[0:int(size / 3), 0:int(size / 3)]
+    A12 = matrix_example[0:int(size / 3), int(size / 3):int(2 * size / 3)]
+    A13 = matrix_example[0:int(size / 3), int(2 * size / 3):int(size)]
+    A21 = matrix_example[int(size / 3):int(2 * size / 3), 0:int(size / 3)]
+    A22 = matrix_example[int(size / 3):int(2 * size / 3), int(size / 3):int(2 * size / 3)]
+    A23 = matrix_example[int(size / 3):int(2 * size / 3), int(2 * size / 3):int(size)]
+    A31 = matrix_example[int(2 * size / 3):int(size), 0:int(size / 3)]
+    A32 = matrix_example[int(2 * size / 3):int(size), int(size / 3):int(2 * size / 3)]
+    A33 = matrix_example[int(2 * size / 3):int(size), int(2 * size / 3):int(size)]
+    return A11, A12, A13, A21, A22, A23, A31, A32, A33
+
 
 def stand_mnoz(A, B):
     if len(A.T) != len(B):
@@ -67,19 +57,24 @@ def stand_mnoz(A, B):
                 result[i, j] = sum(A[i]*(B.T[j]).T)
         return result
 
+def save_stand_mnoz_to_file(matrix_A, matrix_B, filepath): # funkcja zapisująca macierz do pliku
+    f = open(filepath, "w")  # "w" oznacza, że jeżeli plik nie isnieje, to zostanie utworozny, jeżeli istnieje, to go nadpiszemy
+    f.write(str(stand_mnoz(matrix_A, matrix_B))) #tutaj akurat zapisuję macierz powstałą w funkcji pomocniczej
+    f.close()
+    return
 
 #Przykład
 A = load_matrix_B_from_file("matrix_example_1.txt")
 B = load_matrix_A_from_file("matrix_example_2.txt")
-print(A)
-print(adjust_size(A))
-print(B)
-print(adjust_size(B))
-print(is_square(A))
-print(is_square(B))
-save_matrix_to_file(A,"adjust_size_example_1")
+# print(A)
+# print(adjust_size(A))
+# print(B)
+# print(adjust_size(B))
+# print(is_square(A))
+# print(is_square(B))
+# save_stand_mnoz_to_file(A, B,"stand_mnoz_example_1")
 print(split(adjust_size(B)))
 
 C=matrix([[1,0,0],[0,1,0],[0,0,1]])
 D=matrix([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
-print(stand_mnoz(C,D))
+# print(stand_mnoz(C,D))
