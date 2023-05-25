b45d9a532df61ef0300963c27bb6d37d0b12d855
Sabina <sabinaduchnowska@gmail.com>
2023-05-25 16:16:17 +0200

    Dodałam funkcję generującą dowolnie duże macierze kwadratowe z liczb całkowitych

---
+++ matrix_generator.py
@@ -0,0 +1,18 @@
+import numpy
+from functions import *
+
+
+def matrix_generator(n):
+    return numpy.random.randint(0, 100, (n, n))
+
+
+def save_generated_matrix_to_file(n, filepath):
+    A = matrix_generator(n)
+    f = open(filepath, "w")
+    for i in range(len(A)):
+        f.write(' '.join(map(str, A[i])) + '\n')
+    f.close()
+    return
+
+
+save_generated_matrix_to_file(30, "generated_matrix_1.txt")
