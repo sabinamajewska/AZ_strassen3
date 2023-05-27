a979048d4ae537b550aedb6b79717aebb25a8ae5
Sabina <sabinaduchnowska@gmail.com>
2023-05-27 14:58:49 +0200

    Dodałam instrukcję obsługo programu

--- App/app.py
+++ App/app.py
@@ -1,5 +1,8 @@
 from Algoritms.load_and_safe_matrix import *
 
+# INSTRUKCJA OBSŁUGI PROGRAMU
+# Wpisz macierze, które chcesz pomnożyć, pierwszą macierz zapisz do pliku App/matrix_A.txt, a drugą do App/matrix_B.txt.
+# Następnie uruchom plik app.py, wynik zostanie zapisany w pliku App/multiplication_result.txt
 
 def app():
     A = load_matrix_from_file("../App/matrix_A.txt")
