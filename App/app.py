6d4f609468752a464cefa74d7f2e4e9adbb3bf18
Sabina <sabinaduchnowska@gmail.com>
2023-05-27 15:00:49 +0200

    Zrobiłam poprawki stylistyczne pliku app.py

--- App/app.py
+++ App/app.py
@@ -1,12 +1,15 @@
 from Algoritms.load_and_safe_matrix import *
 
+
 # INSTRUKCJA OBSŁUGI PROGRAMU
 # Wpisz macierze, które chcesz pomnożyć, pierwszą macierz zapisz do pliku App/matrix_A.txt, a drugą do App/matrix_B.txt.
 # Następnie uruchom plik app.py, wynik zostanie zapisany w pliku App/multiplication_result.txt
+
 
 def app():
     A = load_matrix_from_file("../App/matrix_A.txt")
     B = load_matrix_from_file("../App/matrix_B.txt")
     save_multiply_matrices_to_file(A, B, "../App/multiplication_result.txt")
+
 
 app()
