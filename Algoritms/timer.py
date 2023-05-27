bcc187a6988282d0287f3bd9e3ba181ee8782c7a
Sabina <sabinaduchnowska@gmail.com>
2023-05-27 15:02:52 +0200

    Zrobiłam poprawki stylistyczne pliku timer.py

--- Algoritms/timer.py
+++ Algoritms/timer.py
@@ -3,10 +3,10 @@ import time
 
 
 def time_it(A, B):
-    start_stand_mnoz = time.time()
-    stand_result = stand_multiplycation(A, B)
-    end_stand_mnoz = time.time()
+    start_stand_multiplycation = time.time()
+    stand_multiplycation(A, B)
+    end_stand_multiplycation = time.time()
     start_multiply_matrices_strassen = time.time()
-    strassen_result = multiply_matrices_strassen(A, B)
+    multiply_matrices_strassen(A, B)
     end_multiply_matrices_strassen = time.time()
-    return [end_stand_mnoz - start_stand_mnoz, end_multiply_matrices_strassen - start_multiply_matrices_strassen]
+    return [end_stand_multiplycation - start_stand_multiplycation, end_multiply_matrices_strassen - start_multiply_matrices_strassen]
