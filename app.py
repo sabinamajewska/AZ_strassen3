7682584ac9e68034e39450d32bde9ea30679ef03
Sabina <sabinaduchnowska@gmail.com>
2023-06-15 11:46:34 +0200

    Zmieniłam lokalizację plików app.py matrix_generator.py i ścieżki do plików

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from Algoritms.load_and_safe_matrix import *
 
 
 def app():
-    A = load_matrix_from_file("../App/matrix_A.txt")
-    B = load_matrix_from_file("../App/matrix_B.txt")
-    save_multiply_matrices_to_file(A, B, "../App/multiplication_result.txt")
+    A = load_matrix_from_file("App/matrix_A.txt")
+    B = load_matrix_from_file("App/matrix_B.txt")
+    save_multiply_matrices_to_file(A, B, "App/multiplication_result.txt")
 
 
 app()
